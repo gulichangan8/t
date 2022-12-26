58463ad8b2d3ceab78452b0fae3c1191e081c631
故里长安 <2845321730@qq.com>
2022-12-26 23:20:48 +0800

    修改了并发b中的问题

--- 并发b.go
+++ 并发b.go
@@ -7,27 +7,37 @@ import (
 )
 
 var wg sync.WaitGroup
+var channels [3]chan int
 
 func main() {
-	var channel = make(chan bool)
+
+	var ch1 = make(chan int)
+	var ch2 = make(chan int)
+	var ch3 = make(chan int)
+	channels[0] = ch1
+	channels[1] = ch2
+	channels[2] = ch3
+
 	wg.Add(3)
-	go Work("goroutine1", channel)
-	go Work("goroutine2", channel)
-	go Work("goroutine3", channel)
+	go work("goroutine1", 0)
+	go work("goroutine2", 1)
+	go work("goroutine3", 2)
+	ch1 <- 1
 	wg.Wait()
 	fmt.Println("successful")
 }
 
-func Work(workName string, channel chan bool) {
-	var c = true
-	channel <- c
-	close(channel)
-	time.Sleep(time.Second) // 模拟逻辑处理
+func work(workName string, index int) {
+	var ch = channels[index]
+	<-ch
+	close(ch)
+
+	time.Sleep(time.Second)
 	fmt.Println(workName)
 	wg.Done()
-}
 
-//goroutine1
-//goroutine2
-//goroutine3
-//successful
+	index = index + 1
+	if index < len(channels) {
+		channels[index] <- index
+	}
+}
